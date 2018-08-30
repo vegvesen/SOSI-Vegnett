63a3d001421ade203f212d188b958c2eaf4c91b3
jetgeo <knut@jetlundweb.no>
2018-08-30 15:29:41 +0200

    Fjerna gammel eksempelmappe

--- Eksempler/runshapechange.bat
+++
@@ -1,1 +0,0 @@
-"java" -Dfile.encoding=UTF-8 -Dhttp.proxyHost=proxy.vegvesen.no -Dhttp.proxyPort=8080 -jar "C:\Program Files (x86)\Arkitektum AS\ShapeChange\ShapeChange.jar" -c "C:\DATA\GitHub\SOSI-Vegnett\Eksempler\config\ShapeChangeConfiguration.xml"
